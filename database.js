25565334b772df0fcc2dc313d21baed6c14d1a49
Rafael <rafaelmota23666@gmail.com>
2026-09-06 22:34:06 -0300

    Usar banco persistente no volume

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./oficina.db', (err) => {
+const db = new sqlite3.Database('./data/oficina.db', (err) => {
     if (err) {
         console.error('Erro ao conectar ao banco:', err.message);
     } else {
